7c1752084f799cb72a1fb5b801841e09fe32bac3
liumeng <liumeng0403@163.com>
2018-02-23 22:23:30 +0800

    [bug] spring bean 重名冲突 解决

--- lm-spring/solution/pojo/src/main/java/lm/solution/model/jaxws/SquareRootServer.java
+++ lm-spring/solution/pojo/src/main/java/lm/solution/model/jaxws/SquareRootServer.java
@@ -1,9 +1,12 @@
 package lm.solution.model.jaxws;
 
+import org.springframework.stereotype.Service;
+
 import javax.jws.WebMethod;
 import javax.jws.WebService;
 import javax.jws.soap.SOAPBinding;
 
+@Service("squareRootServer")
 @WebService
 @SOAPBinding(style = SOAPBinding.Style.RPC)
 public interface SquareRootServer {
--- lm-spring/solution/pojo/src/main/java/lm/solution/model/jaxwsri/SquareRootServer.java
+++ lm-spring/solution/pojo/src/main/java/lm/solution/model/jaxwsri/SquareRootServer.java
@@ -1,10 +1,13 @@
 package lm.solution.model.jaxwsri;
 
+import org.springframework.stereotype.Service;
+
 import javax.jws.WebMethod;
 import javax.jws.WebParam;
 import javax.jws.WebService;
 import javax.jws.soap.SOAPBinding;
 
+@Service("squareRootServerRI")
 @WebService
 @SOAPBinding(style = SOAPBinding.Style.RPC)
 public interface SquareRootServer {

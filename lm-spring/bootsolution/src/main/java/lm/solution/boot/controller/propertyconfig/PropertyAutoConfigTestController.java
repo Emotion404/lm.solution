d3150a458ffa88fc35510ca5fc0309d8e8584e6c
liumeng <liumeng@yiguo.com>
2018-04-23 17:42:54 +0800

    [mod] 升级 spring boot 版本 1.3.0.RELEASE --> 1.5.12.RELEASE ，并解决 property 自动配置问题

--- lm-spring/bootsolution/src/main/java/lm/solution/boot/controller/propertyconfig/PropertyAutoConfigTestController.java
+++ lm-spring/bootsolution/src/main/java/lm/solution/boot/controller/propertyconfig/PropertyAutoConfigTestController.java
@@ -1,4 +1,4 @@
-package lm.solution.boot.controller;
+package lm.solution.boot.controller.propertyconfig;
 
 import lm.solution.boot.config.property.AuthorSettings;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,18 +7,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-public class RestController {
+public class PropertyAutoConfigTestController {
 
     @Autowired
     private AuthorSettings authorSettings;
 
-    @RequestMapping("/")
+    @RequestMapping("/propertyconfig")
     @ResponseBody
     public String index() {
 
         return "author name is "+authorSettings.getName()+" and author age is "+authorSettings.getAge();
-        //return "book name is:" + bookName + " and author is:" + bookAuthor;
-        //return "Hello Spring Boot";
 
     }
 

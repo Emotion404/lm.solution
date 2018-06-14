c4c77d93bdd3ffe234832c07be75a3866a97e570
l.m.2018 <liumeng0403@163.com>
2018-06-14 22:43:04 +0800

    [add] C++ sizeof

---
+++ CAndCppSolution/C++ 入门经典 第6版/P017/P017/Sizer.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "The size of bool:" << sizeof(bool) << endl;
+	cout << "The size of char:" << sizeof(char) << endl;
+	cout << "The size of short:" << sizeof(short) << endl;
+	cout << "The size of int:" << sizeof(int) << endl;
+	cout << "The size of long:" << sizeof(long) << endl;
+	cout << "The size of float:" << sizeof(float) << endl;
+	cout << "The size of double:" << sizeof(double) << endl;
+	cout << "The size of (long long int)" << sizeof(long long int) << endl;
+
+	return 0;
+}

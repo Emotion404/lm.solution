9127deb0b689ffa9bfad2dc4abe279212cfb9e87
l.m.2018 <liumeng0403@163.com>
2018-06-14 23:24:01 +0800

    [add] C++ "="

---
+++ CAndCppSolution/C++ 入门经典 第6版/P021/P021/Rectangle.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	unsigned short width = 26, length;
+	length = 40;
+
+	unsigned short area = width * length;
+
+	cout << "width:" << width<<endl;
+	cout << "length:" << length << endl;
+	cout << "area:" << area << endl;
+
+	return 0;
+}

50b8e674cae28919e8e734d662b8989812221a1d
liumeng <liumeng0403@163.com>
2018-07-01 23:07:47 +0800

    [add] VC++ win32 窗体示例 part01

---
+++ CAndCppSolution/Visual C++ 程序设计及实践/P126/P126/SimpleWindow.cpp
@@ -0,0 +1,17 @@
+#include <Windows.h>
+#include <tchar.h>
+
+LRESULT CALLBACK WindowProc(
+							HWND hwnd,
+							UINT uMsg,
+							WPARAM wParam,
+							LPARAM lParam);
+
+int WINAPI WinMain( 
+				   HINSTANCE hInstance,
+				   HINSTANCE hPreInstance,
+				   LPSTR lpCmdLine,
+				   int nCmdShow)
+{
+	
+}

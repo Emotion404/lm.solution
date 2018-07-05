cde20fb852774bdeb69953e53bbaa3a0fb3423a7
liumeng <liumeng0403@163.com>
2018-07-05 20:05:29 +0800

    [add] VC++ MFC CString

--- CAndCppSolution/Visual C++ 程序设计及实践/P140/P140/CMyApp.cpp
+++ CAndCppSolution/Visual C++ 程序设计及实践/P140/P140/CMyApp.cpp
@@ -22,7 +22,27 @@ BOOL CMyApp:: InitInstance()  // 覆盖继承的应用程序初始化虚函数
 		AfxMessageBox(_T("用户单击了'忽略'按钮"),MB_OK|MB_ICONINFORMATION		  );
 	}
 
-	// 
+	// 连接字符串
+	CString str1(_T("美丽的"));
+	CString str2(_T("海滨城市"));
+	CString str3=str1+str2;
+	str3=str3+_T("青岛");
+	AfxMessageBox(str3);
+
+	// 查找字符串
+	nRet=str3.Find(str2);
+	if(nRet>=0)
+	{
+		AfxMessageBox(_T("找到字符串"));
+	}
+	else
+	{
+		AfxMessageBox(_T("没有匹配的字符串"));
+	}
+
+	// 提取字符串
+	CString str4=str1.Left(2)+str3.Mid(3,2)+str2.Right(2);
+	AfxMessageBox(_T("提取的字符串为:" )+str4);
 
 	return TRUE;
 }

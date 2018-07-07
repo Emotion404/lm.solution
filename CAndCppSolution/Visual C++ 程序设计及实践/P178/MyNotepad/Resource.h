8dd1f59dfaf35997efae0c453cbe6bf86b530eac
liumeng <liumeng0403@163.com>
2018-07-08 02:02:40 +0800

    [add] VC++ MFC 快捷键 Alt+  Ctrl+

--- CAndCppSolution/Visual C++ 程序设计及实践/P178/MyNotepad/Resource.h
+++ CAndCppSolution/Visual C++ 程序设计及实践/P178/MyNotepad/Resource.h
@@ -22,7 +22,7 @@
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        311
-#define _APS_NEXT_COMMAND_VALUE         32780
+#define _APS_NEXT_COMMAND_VALUE         32782
 #define _APS_NEXT_CONTROL_VALUE         1000
 #define _APS_NEXT_SYMED_VALUE           310
 #endif

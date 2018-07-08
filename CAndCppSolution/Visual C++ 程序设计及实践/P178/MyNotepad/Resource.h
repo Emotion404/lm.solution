599686ae8dc5910a4e55bcdbea23963f95403565
liumeng <liumeng0403@163.com>
2018-07-09 01:13:44 +0800

    [add] VC++ MFC SDI toolbar

--- CAndCppSolution/Visual C++ 程序设计及实践/P178/MyNotepad/Resource.h
+++ CAndCppSolution/Visual C++ 程序设计及实践/P178/MyNotepad/Resource.h
@@ -7,7 +7,6 @@
 #define IDR_MAINFRAME                   128
 #define IDR_MyNotepadTYPE               130
 #define IDB_BITMAP1                     310
-#define O                               311
 #define IDR_MYPOP_MENU                  311
 #define ID_32771                        32771
 #define ID_32772                        32772
@@ -21,13 +20,14 @@
 #define ID_32782                        32782
 #define ID_32783                        32783
 #define ID_32784                        32784
+#define ID_BUTTON32785                  32785
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        312
-#define _APS_NEXT_COMMAND_VALUE         32785
+#define _APS_NEXT_COMMAND_VALUE         32786
 #define _APS_NEXT_CONTROL_VALUE         1000
 #define _APS_NEXT_SYMED_VALUE           310
 #endif

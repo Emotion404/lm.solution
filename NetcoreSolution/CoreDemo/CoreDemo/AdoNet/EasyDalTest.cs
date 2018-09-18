39f8ffcd15644f265f4dd06bbf2006d4b451bc6d
liumeng <liumeng0403@163.com>
2018-09-18 19:57:41 +0800

    [add]  books

---
+++ NetcoreSolution/CoreDemo/CoreDemo/AdoNet/EasyDalTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+using CoreDemo.AttributesDemo;
+using EasyDAL.Exchange;
+
+namespace CoreDemo.AdoNet
+{
+    public class EasyDalTest
+    {
+
+        private IDbConnection Conn { get; set; }
+
+        public async void Test01()
+        {
+
+            //await Conn
+            //    .Selecter<DbEntity>()
+            //    .Where(it => it.Name == "xx")
+            //    .
+
+        }
+
+    }
+}
